c0d03e1bbc39eab80ec67b2a4ed7c4e7d8b5fdb7
smyyoztrk <s.kaya@msn.com>
2024-07-10 18:36:46 +0300

    conftest.py dosyasına -@pytest.fixture()- satırı eklendi

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,18 @@ import pytest
 from constants import globalconstants as c
 from pages.anasayfa import Anasayfa
 
-@pytest.fixture(scope="class")
+# @pytest.fixture(scope="class") # class başında bir kere çalışır tüm testler koşar kapanır.
+@pytest.fixture() # her test için çalışır
 def setup(request):
     driver=webdriver.Chrome()
     driver.get(c.BASE_URL)
     driver.maximize_window()
     request.cls.driver = driver # bu fixture kullanan class lar driver çağırdığında driver = webdriver.Chrome() u ver dedik.  
     anasayfa = Anasayfa(driver)
-    anasayfa.adres_arama_cubuguna_adres_gir()
-
+    try:
+        anasayfa.adres_arama_cubuguna_adres_gir()
+    except:
+        pass
     anasayfa.anasayfada_restoran_arat(restoranAdi="sarmaşık kebap")
     anasayfa.restoran_sec_sarmasik_cart_sayfasi_icin()
     yield
